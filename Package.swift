9f9f11361e0533891ff0bb3d8ad658ee75a7d6a7
Steven Klassen <klassens@acm.org>
2022-06-02 11:47:36 -0600

    Temporary change of dependencies until KSSCore updates are done

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
         .library(name: "CrestLib", targets: ["CrestLib"])
     ],
     dependencies: [
-        .package(url: "https://github.com/klassen-software-solutions/KSSCore.git", from: "5.1.1"),
+        //.package(url: "https://github.com/klassen-software-solutions/KSSCore.git", from: "5.1.1"),
+        .package(url: "https://github.com/klassen-software-solutions/KSSCore.git", .branch("development/v5")),
+        //.package(url: "https://github.com/klassen-software-solutions/KSSCore.git", .revision("244f0c549f90eb4da0d88111d2544ca15ba56143")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2"),
         .package(url: "https://github.com/swift-server/async-http-client", from: "1.10.0"),
         .package(url: "https://github.com/Kitura/Configuration.git", from: "3.0.200")
